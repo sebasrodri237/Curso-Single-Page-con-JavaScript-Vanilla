79019d84df325c7864eb625369c3ff1a7ef5190a
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-27 22:41:08 -0500

    Clase 5 - Crear el Home

---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+const home = () => {
+//Template para generar la vista de los personajes en el home
+    const view = `
+        <div class = "characters">
+            <article class = "characters-item">
+                <a href = "#/1/">
+                    <img src = "image" alt = "name">
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+    `;
+
+    return view;
+}
+
+export default home;
+//exportamos la funcion para poder reutilizarla en otras partes si es necesario
