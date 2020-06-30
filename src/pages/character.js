50b465a0e023d61220f9476cfd9c7d383b432de4
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-29 23:14:34 -0500

    Clase 12 - Conectar la funcion con la descripcion de los personajes

--- src/pages/character.js
+++ src/pages/character.js
@@ -1,21 +1,25 @@
 //Este a diferencia de header si es una pagina
-//Aqui se construye la vista de un solo personaje al darle click
-const character = () => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
 
+const character = async () => {
+//Aqui se construye la vista de un solo personaje al darle click
+    const id = getHash();//Obtenemos el id
+    const character = await getData(id);
     const view = `
     
     <div class="characters-inner">
         <article class="characters-card">
-            <img src="image" alt="name">
-            <h2>Name</h2>
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
         </article>
         <article class="characters-card">
-            <h3>Episodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender:</h3>
-            <h3>Origin:</h3>
-            <h3>Last Location:</h3>
+            <h3>Episodes:<span>${character.episode.length}</span></h3>
+            <h3>Status:<span>${character.status}</span></h3>
+            <h3>Species:<span>${character.species}</span></h3>
+            <h3>Gender:<span>${character.gender}</span></h3>
+            <h3>Origin:<span>${character.origin.name}</span></h3>
+            <h3>Last Location:<span>${character.location.name}</span></h3>
         </article>
     </div>
     `;
